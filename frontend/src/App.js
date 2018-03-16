1355bf3381d93a5c97d42ae4891e14c73d5662a2
mahadyC <mahady.chowdhury@metropolia.fi>
2018-03-16 12:11:09 +0200

    Import the function from the api file and test if it is able to fetch the data from the server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
-
+import * as API from './APIs'
 
 class App extends Component {
 
+  componentDidMount() {
+    API.getCategories().then(categories => console.log(categories))
+  }
   render() {
     return (
       <div className="App">
